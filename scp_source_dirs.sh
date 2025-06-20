47e852cdbac91c87ac74c0c07e992a52657203bb
Tomohiro MOTODA <motoda.populus14144@gmail.com>
2025-06-20 17:49:34 +0900

    Add scp_source_dirs.sh script for transferring unique directories from JSON

---
+++ scp_source_dirs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usage: ./scp_source_dirs.sh <user>@<host>:/remote/path [source_files.json]
+# Example: ./scp_source_dirs.sh user@192.168.1.100:/home/user/data
+
+REMOTE_DEST="$1"
+SOURCE_JSON="${2:-analysis_result.json}"
+
+if [ -z "$REMOTE_DEST" ]; then
+  echo "Usage: $0 <user>@<host>:/remote/path [source_files.json]"
+  exit 1
+fi
+
+# Extract unique directories from source_files in the JSON
+DIRS=$(jq -r '.source_files[]' "$SOURCE_JSON" | sort | uniq)
+
+for dir in $DIRS; do
+  echo "Sending $dir to $REMOTE_DEST"
+  scp -r "$dir" "$REMOTE_DEST"
+done
